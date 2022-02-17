e6e493ff5e41549b8209613b7dabc75913962d2a
avendo <namankaushikplt@gmail.com>
2022-02-17 22:38:59 +0530

    - Extracted constants - Removed the settings method as it is unnecessary

--- src/main/java/MovingBall.java
+++ src/main/java/MovingBall.java
@@ -6,36 +6,43 @@ public class MovingBall extends PApplet {
 
     public static final int BACKGROUND_COLOR = 100;
 
+    public static final int SCREEN_WIDTH = getScreenSize().width / 2;
+    public static final int SCREEN_HEIGHT = getScreenSize().height / 2;
+
     public static void main(String[] args) {
         PApplet.main("MovingBall", args);
     }
 
-    @Override
+    /*@Override                                             //Only used when necessary like in Eclipse
     public void settings() {
         super.settings();
-        size(getScreenSize().width/2, getScreenSize().height/2);
-    }
-
-    private Dimension getScreenSize() {
-        return Toolkit.getDefaultToolkit().getScreenSize();
-    }
+        size(SCREEN_WIDTH, SCREEN_HEIGHT);
+    }*/
 
     @Override
-    public void setup() {
+    public void setup() {                       //Used at most places
+        int xCoordinate = SCREEN_WIDTH / 2;
+        int yCoordinate = SCREEN_HEIGHT / 2;
+
+        size(SCREEN_WIDTH, SCREEN_HEIGHT);
+
         drawBackground(0);
-        int xCoordinate = getScreenSize().width / 4;
-        int yCoordinate = getScreenSize().height / 4;
+
         ellipse(xCoordinate, yCoordinate, 100, 100);
     }
 
     @Override
     public void draw() {
         drawBackground(BACKGROUND_COLOR);
-        ellipse(mouseX,100,100,100 );
+        ellipse(mouseX, 100, 100, 100);
     }
 
     private void drawBackground(int backgroundColor) {
         background(backgroundColor);
+    }
+
+    private static Dimension getScreenSize() {
+        return Toolkit.getDefaultToolkit().getScreenSize();
     }
 
 }
